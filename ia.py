165069f824617a670649c920e9c83e30df9d3a9e
Nathan Billaud <natbil@orange.fr>
2025-06-13 21:48:43 +0200

    début de l'ajout de la fonction du bot

--- ia.py
+++ ia.py
@@ -1,15 +1,37 @@
 import customtkinter as ctk
 
-#Réponse
+# Envoie de la quesion
+def send(event=None):
+    question = entry.get()
+    bot_resp = response(entry.get())
+    if question.strip() != "":
+        entry.delete(0, ctk.END)
+        chat_history.configure(state="normal")
+        chat_history.insert("end", f"Vous: {question}\n", "user")
+        chat_history.insert("end", f"IA: {bot_resp}\n", "bot")
+        chat_history.configure(state="disabled")
+
+# Réponse
 def response(user_input):
+    nbWordsKnown = 0
+    nbWordsKnownMax = 0
+    rightQuestion = ""
     responses = {
-        "Bonjour": "Bonjour, ça va ?",
-        "tu es une IA ?": "oui, mais selon certains je deviendrai vite meilleur que toi, sombre idiot !",
-        "Au revoir": "A plus !"
+        "bonjour": "Bonjour, ça va ?",
+        "tu es une ia ?": "oui, mais selon certains je deviendrai vite meilleur que toi, sombre idiot !",
+        "au revoir": "A plus !"
     }
-    user_input = user_input.strip()
-    if user_input in responses:
-        return responses[user_input]
+    for knownQuestion in responses.keys():
+        for word in user_input.strip():
+            if word in knownQuestion:
+                nbWordsKnown+=1
+        if nbWordsKnown > nbWordsKnownMax:
+            nbWordsKnownMax = nbWordsKnown
+            rightQuestion = knownQuestion
+        nbWordsKnown = 0
+
+    if user_input.lower() in responses:
+        return responses[rightQuestion]
     else:
         return "Désolé, je ne comprends pas votre demande."
 
@@ -26,7 +48,7 @@ header.pack(pady=10)
 # Création de l'affichage des messages
 chat_history = ctk.CTkTextbox(app, width=480, height=450, state="disabled")
 chat_history.pack(pady=10, padx=10, fill="both", expand=True)
-#Ajout des couleurs pour le bot et le user
+# Ajout des couleurs pour le bot et le user
 chat_history.tag_config("user", foreground="blue")
 chat_history.tag_config("bot", foreground="red")
 
@@ -37,7 +59,11 @@ user_input.pack(pady=10, padx=10, fill="x")
 entry = ctk.CTkEntry(user_input, placeholder_text="Entrez votre texte ici !", width=400)
 entry.pack(padx=5, side="left")
 
-send = ctk.CTkButton(user_input, text="Send", cursor="hand2")
-send.pack(side="right")
+sendButton = ctk.CTkButton(user_input, text="Send", cursor="hand2", command=send)
+sendButton.pack(side="right")
+
+# Associer la touchee entrée à l'envoie du message
+app.bind("<Return>", send)
+
 
 app.mainloop()
