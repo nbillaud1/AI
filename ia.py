0846353c7d8cc527d22854d718acb45f0dd1ca66
Nathan Billaud <natbil@orange.fr>
2025-06-14 18:25:31 +0200

    ajout de updateKnowladge

--- ia.py
+++ ia.py
@@ -4,22 +4,34 @@ import re
 useless_words = []
 responses = {}
 
+#TODO ajouter le : Ai-je bien répondu ? et l'update des connaissances associé.
+
 # Connaissances de base
 # bonjour:Bonjour, que puis-je faire pour vous ?
 # au revoir:Au revoir !
-# tu est une ia:Absolument, une Infinie Alcoolique !
+# tu est une ia:une Infinie Alcoolique !
 # ok:Y'a t'il autre chose que je peux faire pour vous ?
 # accord:Y'a t'il autre chose que je peux faire pour vous ?
 # merci:Y'a t'il autre chose que je peux faire pour vous ?
 
-with open("inutiles.txt", "r") as i:
-    useless_words = i.read().split()
+def updateKnowledge(addInI=None, addInC=None):
+    """addInI doit être une chaîne de caractère et addInC une liste de deux chaînes: [question,réponse]"""
 
-with open("connaissances.txt", "r") as c:
-    for line in c:
-        responses[line.split(":")[0]] = line.split(":")[1]
+    # Chargement des variables (lecture du fichier)
+    with open("inutiles.txt", "r") as i:
+            useless_words = i.read().split()
 
-def updateKnowledge():
+    with open("connaissances.txt", "r") as c:
+        for line in c:
+            responses[line.split(":")[0]] = line.split(":")[1]
+
+    # Ajout de connaissances dans les variables
+    if not addInI == None:
+        useless_words.append(addInI)
+    if not addInC == None:
+        responses[addInC[0]] = addInC[1]
+
+    # Ecriture dans les fichiers
     with open("inutiles.txt", "w") as i:
         knowledge = ""
         for word in useless_words:
@@ -35,7 +47,7 @@ def updateKnowledge():
                 knowledge += question + ":" + responses[question]
         c.write(knowledge)
 
-# Envoie de la quesion
+# Envoie de la quesion + affichage réponse
 def send(event=None):
     question = entry.get()
     bot_resp = response(entry.get().lower())
@@ -54,7 +66,7 @@ def response(user_input):
         rightQuestion = ""
         for knownQuestion in responses.keys():
             for word in re.split(r"[ \'-]", user_input): # pour split selon plusieurs critères
-                if word in knownQuestion and word not in useless_words:
+                if word.strip(".?!,") in knownQuestion and word.strip(".?!,") not in useless_words:
                     nbWordsKnown+=1
             if nbWordsKnown > nbWordsKnownMax:
                 nbWordsKnownMax = nbWordsKnown
@@ -69,7 +81,7 @@ def response(user_input):
         updateKnowledge()
         app.destroy()
 
-
+updateKnowledge()
 # Création de l'app
 app = ctk.CTk()
 app.geometry("500x600")
@@ -77,7 +89,7 @@ app.overrideredirect(True)
 app.title("IA (Infiniment Alcoolique)")
 
 # Création de l'en-tête
-header = ctk.CTkLabel(app, text="Bienvenue ! pour quitter entrez /q", font=("Calibri", 20, "bold"))
+header = ctk.CTkLabel(app, text="IA (Infiniment Analphabète) ! pour quitter entrez /q", font=("Calibri", 20, "bold"))
 header.pack(pady=10)
 
 # Création de l'affichage des messages
