5b445836dce2bbe727ed4f1eb5f1e09a337d9d5f
Nathan Billaud <natbil@orange.fr>
2025-06-14 23:45:03 +0200

    ajout de la demande d'upgrading par l'ia (work in progress)

--- ia.py
+++ ia.py
@@ -50,13 +50,19 @@ def updateKnowledge(addInI=None, addInC=None):
 # Envoie de la quesion + affichage réponse
 def send(event=None):
     question = entry.get()
-    bot_resp = response(entry.get().lower())
+    bot_resp = response(question.lower())
     if question.strip() != "":
-        entry.delete(0, ctk.END)
-        chat_history.configure(state="normal", font=(("Calibri", 18, "normal")))
-        chat_history.insert("end", f"Vous: {question}\n", "user")
-        chat_history.insert("end", f"IA: {bot_resp}\n", "bot")
-        chat_history.configure(state="disabled")
+        show_user(question)
+        show_bot(bot_resp)
+        if bot_resp != "Désolé, je ne comprends pas votre demande.\n":
+            show_bot("Ai-je bien répondu ? (oui / non)\n")
+            entry.get()
+            app.bind("<Return>", confirmation_correction(question))
+            #sendButton = ctk.CTkButton(user_input, text="Send", cursor="hand2", command=confirmation_correction(question))
+        else:
+            show_bot("Voulez-vous m'apprendre à répondre à ce type de question ? (oui / non)\n")
+            app.bind("<Return>", confirmation_learning(question))
+            #sendButton = ctk.CTkButton(user_input, text="Send", cursor="hand2", command=confirmation_learning(question))
 
 # Réponse
 def response(user_input):
@@ -76,17 +82,70 @@ def response(user_input):
         if rightQuestion != "":
             return responses[rightQuestion]
         else:
-            return "Désolé, je ne comprends pas votre demande."
+            return "Désolé, je ne comprends pas votre demande.\n"
     else:
         updateKnowledge()
         app.destroy()
+
+# Si on veut corriger l'IA
+def confirmation_correction(question, event=None):
+    user_conf = entry.get()
+    if "/q" not in user_conf:
+        show_user(user_conf)
+        if user_conf.lower() == "non":
+            show_bot("Dîtes moi donc ce que je dois répondre\n")
+            #sendButton = ctk.CTkButton(user_input, text="Send", cursor="hand2", command=learn(question, user_conf.lower()))
+            app.bind("<Return>", learn(question, user_conf.lower()))
+        else:
+            show_bot("D'accord !\n")
+            #sendButton = ctk.CTkButton(user_input, text="Send", cursor="hand2", command=send)
+            app.bind("<Return>", send)
+    else:
+        updateKnowledge()
+        app.destroy()
+
+# Si on veut apprendre à l'IA
+def confirmation_learning(question, event=None):
+    user_conf = entry.get()
+    if "/q" not in user_conf:
+        show_user(user_conf)
+        if user_conf.lower() == "oui":
+            show_bot("Dîtes moi donc ce que je dois répondre\n")
+            #sendButton = ctk.CTkButton(user_input, text="Send", cursor="hand2", command=learn(question, user_conf.lower()))
+            app.bind("<Return>", learn(question, user_conf.lower()))
+        else:
+            show_bot("D'accord !\n")
+            #sendButton = ctk.CTkButton(user_input, text="Send", cursor="hand2", command=send)
+            app.bind("<Return>", send)
+    else:
+        updateKnowledge()
+        app.destroy()
+
+def learn(answer, question, event=None):
+    print("coucou")
+    updateKnowledge(addInC=[question, answer])
+    show_bot("Les informations sont bien enregistrées !")
+    #sendButton = ctk.CTkButton(user_input, text="Send", cursor="hand2", command=send)
+    app.bind("<Return>", send)
+
+
+def show_user(text):
+    chat_history.configure(state="normal", font=(("Calibri", 18, "normal")))
+    chat_history.insert("end", f"Vous: {text}\n", "user")
+    entry.delete(0, ctk.END)
+    chat_history.configure(state="disabled")
+
+def show_bot(text):
+    chat_history.configure(state="normal", font=(("Calibri", 18, "normal")))
+    chat_history.insert("end", f"IA: {text}", "bot")
+    chat_history.configure(state="disabled")
 
 updateKnowledge()
 # Création de l'app
 app = ctk.CTk()
 app.geometry("500x600")
 app.overrideredirect(True)
-app.title("IA (Infiniment Alcoolique)")
+app.title("IA (Infiniment Analphabète)")
 
 # Création de l'en-tête
 header = ctk.CTkLabel(app, text="IA (Infiniment Analphabète) ! pour quitter entrez /q", font=("Calibri", 20, "bold"))
@@ -97,7 +156,7 @@ chat_history = ctk.CTkTextbox(app, width=480, height=450, state="disabled")
 chat_history.pack(pady=10, padx=10, fill="both", expand=True)
 # Ajout des couleurs pour le bot et le user
 chat_history.tag_config("user", foreground="blue")
-chat_history.tag_config("bot", foreground="red")
+chat_history.tag_config("bot", foreground="green")
 
 # Création du champs de saisie
 user_input = ctk.CTkFrame(app)
@@ -111,6 +170,5 @@ sendButton.pack(side="right")
 
 # Associer la touchee entrée à l'envoie du message
 app.bind("<Return>", send)
-
 
 app.mainloop()
